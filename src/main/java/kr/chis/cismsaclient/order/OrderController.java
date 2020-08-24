cc44ef6c60be2b04f1892108db877197c529a56d
CIS-Mac_BW <kr.chis@gmail.com>
2020-08-24 09:13:24 +0900

    주문 컨트롤러

--- src/main/java/kr/chis/cismsaclient/order/OrderController.java
+++ src/main/java/kr/chis/cismsaclient/order/OrderController.java
@@ -7,11 +7,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 /**
  * @author InSeok
  * Date : 2020-08-24
- * Remark :
+ * Remark :  주문 컨트롤러
  */
 @Controller
 @RequestMapping("/order")
 public class OrderController {
+
     @GetMapping("")
     public String index(){
         return "/order/index";

ac2edc43327c5814c674979f3e8d5cbfb4cf1548
CIS-Mac_BW <kr.chis@gmail.com>
2020-09-07 10:47:13 +0900

    test Math

--- src/test/java/kr/chis/cismsaclient/service/ShopServiceTest.java
+++ src/test/java/kr/chis/cismsaclient/service/ShopServiceTest.java
@@ -20,4 +20,25 @@ class ShopServiceTest {
     public void shopListTest() {
         shopService.shopList();
     }
+
+    @Test
+    public void testmath(){
+        //System.out.println(Math.pow(81000000,1.0/10.285));
+        //double v = calcPow(8100000000000000000D, 1.0 / 10.285);
+        double v = Math.pow(810000000000000000D, 1.0 / 10.285);
+        //double v = Math.pow(64D,1.0/3.0);
+        //double v = calcPow(4D, 1.0 / 2.0);
+        //Math.pow(2,1.0 / 2.0 );
+        System.out.println(v);
+    }
+
+    public double calcPow(double a, double n){
+        if(n == 0.0){
+            return 1.0;
+        }else{
+            double temp = calcPow(a, n/2);
+            if(n%2 == 0) return temp*temp;
+            else return temp*temp*a;
+        }
+    }
 }
